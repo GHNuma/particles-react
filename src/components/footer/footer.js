3abe8a3543596ef859ffb8be2232aa80b317b1a2
GHNuma <mr.ghnuma@gmail.com>
2022-10-05 12:18:50 +0600

    deploy project

--- src/components/footer/footer.js
+++
@@ -1,9 +0,0 @@
-import { Box, Typography } from "@mui/material";
-
-const Footer = () => (
-  <Box>
-    <Typography variant='h6'>Created by <strong>Rakhymbek + Ramir + Numa</strong></Typography>
-  </Box>
-);
-
-export default Footer;
